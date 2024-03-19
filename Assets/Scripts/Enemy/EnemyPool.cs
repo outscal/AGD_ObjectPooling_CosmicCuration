26fb53365ebff9d6358e33249a29fb7400150276
Roopesh Verma <roopeshv.30@gmail.com>
2024-03-19 20:57:08 +0530

    Add get enemy feature

--- Assets/Scripts/Enemy/EnemyPool.cs
+++ Assets/Scripts/Enemy/EnemyPool.cs
@@ -19,5 +19,29 @@ namespace CosmicCuration.Enemy
             this.enemyView = enemyView;
             this.enemyScriptableObject = enemyScriptableObject;
         }
+
+        public EnemyController GetEnemy()
+        {
+            if (pooledEnemies.Count > 0)
+            {
+                PooledEnemy pooledEnemy = pooledEnemies.Find(item => !item.IsUsed);
+
+                if (pooledEnemy != null)
+                {
+                    pooledEnemy.IsUsed = true;
+                    return pooledEnemy.Enemy;
+                }
+            }
+
+            return CreateNewPooledEnemy();
+        }
+
+        private EnemyController CreateNewPooledEnemy()
+        {
+            PooledEnemy pooledEnemy = new PooledEnemy();
+            pooledEnemy.Enemy = new EnemyController(enemyView, enemyScriptableObject.enemyData);
+            pooledEnemy.IsUsed = false;
+            return pooledEnemy.Enemy;
+        }
     }
 }
